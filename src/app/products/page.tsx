f038dc94c2358dff003526bd62520e7e1ad6aeb3
Sabari Manikandan <imsabarimanikandan@gmail.com>
2023-11-04 15:12:31 +0530

    removed ssr

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,44 +1,64 @@
+"use client"
 import Filters from '@/components/products/Filters';
 import ProductContainer from '@/components/products/ProductContainer';
 import { GET_CATEGORIES_ENDPOINT, GET_PRODUCTS_ENDPOINT } from '@/lib/constants';
 import { noCache } from '@/lib/utils';
 import { ICategory, IProduct } from '@/types/types';
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 
-const products = async () => {
+const Products: React.FC = () => {
 
-    // FETCH PRODUCT DETAILS
-    const response: Response = await fetch(GET_PRODUCTS_ENDPOINT, {
-        cache: noCache,
-        next: {
-            tags: ["products"]
+    const [products, setProducts] = useState<IProduct[]>([])
+    const [categories, setCategories] = useState<ICategory[]>([])
+
+    useEffect(() => {
+        // FETCH PRODUCT DETAILS
+        const fetchProducts = async () => {
+            const response: Response = await fetch(GET_PRODUCTS_ENDPOINT, {
+                cache: noCache,
+                next: {
+                    tags: ["products"]
+                }
+            });
+            const productsData = await response.json() as IProduct[]
+            setProducts(productsData)
         }
-    });
 
-    // FETCH CATEGORIES
-    const categoryResponse: Response = await fetch(GET_CATEGORIES_ENDPOINT, {
-        cache: noCache,
-        next: {
-            tags: ["categories"]
+        const fetchCategories = async () => {
+
+
+            // FETCH CATEGORIES
+            const categoryResponse: Response = await fetch(GET_CATEGORIES_ENDPOINT, {
+                cache: noCache,
+                next: {
+                    tags: ["categories"]
+                }
+            });
+
+            const categoryData = await categoryResponse.json() as ICategory[]
+            setCategories(categoryData)
         }
-    });
 
-    const products = await response.json() as IProduct[];
-    const categories = await categoryResponse.json() as ICategory[];
+        fetchProducts()
+        fetchCategories()
+    }, [])
 
     return (
         <div className='max-w-7xl py-10 mx-auto flex items-center justify-between'>
             {/* FILTER */}
             <div className='grid grid-cols-8 w-full'>
-                <div className='w-full col-span-4 md:col-span-2 pr-10 h-fit sticky top-24'>
-                    <h1 className='font-semibold text-2xl'>Filter</h1>
-                    {/* FILTER LIST */}
-                    <hr className='py-3' />
-                    <div className='space-y-2'>
-                        <Filters categories={categories} />
+                {
+                    categories.length > 0 && <div className='w-full col-span-4 md:col-span-2 pr-10 h-fit sticky top-24'>
+                        <h1 className='font-semibold text-2xl'>Filter</h1>
+                        {/* FILTER LIST */}
+                        <hr className='py-3' />
+                        <div className='space-y-2'>
+                            <Filters categories={categories} />
+                        </div>
                     </div>
-                </div>
+                }
+
 
                 {/* PRODUCT LIST */}
                 <div className='w-full col-span-6 grid md:grid-cols-3 sm:grid-cols-2  gap-4'>
@@ -57,4 +77,4 @@ const products = async () => {
     )
 }
 
-export default products
+export default Products
