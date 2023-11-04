3bc172a78a7b82458c92d73a6276e3c2d5025781
Sabari Manikandan <imsabarimanikandan@gmail.com>
2023-11-04 17:01:51 +0530

    returning 404 from cart api

--- src/app/api/cart/count/route.ts
+++ src/app/api/cart/count/route.ts
@@ -1,15 +1,13 @@
 import { getCartCount, getCartDetails } from "@/lib/services/cart";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: NextRequest) {
+export async function GET(req: NextRequest, res: NextResponse) {
     const sessionId = req.cookies.get("user");
     if (sessionId === undefined) {
-        return Response.json({
-            status: 404
-        })
+        return Response.json({}, { status: 404 })
     }
 
-    const count = await getCartCount(sessionId!.value);
+    const count = await getCartCount(sessionId.value);
 
     return new Response(JSON.stringify(count));
 }
